c38f9e07237de2032713b31fa8a31d2a2e87c6d7
Gray <63938746+piccoloser@users.noreply.github.com>
2024-10-04 17:17:23 -0400

    Update load.rs

--- examples/load.rs
+++ examples/load.rs
@@ -1,6 +1,7 @@
 use config_tools::Config;
 
 fn main() {
-    let config = Config::load("config-sectioned.ini").expect("Failed to load config file");
+    let filename = "config.ini"; // Change as needed
+    let config = Config::load(filename).expect("Failed to load config file");
     println!("{config:#?}");
 }
